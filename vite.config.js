91a266c679ebe4b4a6c5268a2530c699f5481084
revista1919 <revistaestudiantespentauc@gmail.com>
2025-08-12 00:16:11 +0000

    Describe brevemente los cambios realizados

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GH_PAGES ? '/simulacion-pbc-pfp-pmp/' : '/',
+  base: process.env.GITHUB_PAGES ? '/Simulacion-pbc-pfp-pmp-react/' : './'
 })
